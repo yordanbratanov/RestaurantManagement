e2a467600fdda6dd0654494a57fb7c4028af895c
yordan.bratanov@yahoo.com <yordan.bratanov@yahoo.com>
2020-07-27 16:21:47 +0300

    Added attributes to model

--- RestaurantManagement/RestaurantManagement.Entities/Restaurant.cs
+++ RestaurantManagement/RestaurantManagement.Entities/Restaurant.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace RestaurantManagement.Entities
@@ -8,10 +10,14 @@ namespace RestaurantManagement.Entities
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
         
+        [Required]
         public string Location { get; set; }
 
+        [Required]
+        [Range(1,5)]
         public short Rating { get; set; }
     }
 }
